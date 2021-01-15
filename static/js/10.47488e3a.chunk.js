a753324931aa2fc68af01ecc21612e4ac4a08126
Yuliia Kovalenko <kus_queen@icloud.com>
2021-01-15 12:47:33 +0200

    Updates

---
+++ static/js/10.47488e3a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[10],{108:function(t,e,r){"use strict";r.r(e),r.d(e,"default",(function(){return i}));var n=r(1);function i(){return Object(n.jsxs)("div",{role:"alert",children:[Object(n.jsx)("img",{src:"https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg\n",alt:"Error"}),Object(n.jsx)("h1",{style:{textAlign:"center"},children:"Page not found 404"})]})}}}]);
+//# sourceMappingURL=10.47488e3a.chunk.js.map
